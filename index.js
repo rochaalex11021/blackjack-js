0e1527fda13b6da26bd07703efb7ec63cd364033
rochaalex11021 <109445353+rochaalex11021@users.noreply.github.com>
2023-03-09 18:52:12 -0600

    .

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 let firstCard = 10;
-
 let secondCard = 4;
-
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
-
 let message = "";
 
-if (sum <= 20) {
-  message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-  message = "Wohoo! You've got Blackjack!";
-  hasBlackJack = true;
-} else {
-  message = "You're out of the game!";
-  isAlive = false;
-}
+// 1. Store the message-el paragraph in a variable called messageEl
 
-console.log(message);
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+  }
+  // 2. Display the message in the messageEl using messageEl.textContent
+  console.log(message);
+}
