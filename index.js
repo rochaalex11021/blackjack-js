20ff4a4fc9f2d4eb629b1a37258c73a9006c6757
rochaalex11021 <109445353+rochaalex11021@users.noreply.github.com>
2023-03-09 20:23:43 -0600

    .

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
-let sumEl = document.getElementById("sum-el");
+// let sumEl = document.getElementById("sum-el");
+let sumEl = document.querySelector(".sum-el");
+
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+  cardsEl.textContent = "cards: " + firstCard + " " + secondCard;
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
