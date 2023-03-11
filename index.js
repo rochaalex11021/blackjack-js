f058a681b7d6dbfe655d145e8b7e6b1206eb27ce
rochaalex11021 <109445353+rochaalex11021@users.noreply.github.com>
2023-03-10 19:09:34 -0600

    .

--- index.js
+++ index.js
@@ -7,32 +7,37 @@ let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
-
 let cardsEl = document.getElementById("cards-el");
+
+// Create a function, getRandomCard(), that always returns the number 5
 
 function startGame() {
   renderGame();
 }
 
 function renderGame() {
-  cardsEl.textContent = "cards: " + cards[0] + " " + cards[1];
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
   sumEl.textContent = "Sum: " + sum;
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack!";
+    message = "You've got Blackjack!";
     hasBlackJack = true;
   } else {
     message = "You're out of the game!";
     isAlive = false;
   }
-
   messageEl.textContent = message;
 }
 
 function newCard() {
-  console.log("Drawing a new card from the deck!");
-  let card = 7;
+  let card = 6;
   sum += card;
+  cards.push(card);
+  console.log(cards);
   renderGame();
 }
