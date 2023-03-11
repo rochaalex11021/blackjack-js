2ca8fe3d27db2fd4a3950689b45e0400925ba355
rochaalex11021 <109445353+rochaalex11021@users.noreply.github.com>
2023-03-11 09:13:45 -0600

    .

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+console.log(cards);
+
 function getRandomCard() {
-  return Math.floor(Math.random() * 13) + 1;
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
 }
 
 function startGame() {
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
 }
 
@@ -40,6 +51,5 @@ function newCard() {
   let card = getRandomCard();
   sum += card;
   cards.push(card);
-  console.log(cards);
   renderGame();
 }
